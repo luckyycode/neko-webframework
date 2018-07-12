417efcb9f7e55bd05707a03293e314c8d8fb62f1
NekoCode <nekocode@icloud.com>
2018-07-12 19:57:31 +0300

    👐🏻 IControllerContext

---
+++ Mvc/IControllerContext.h
@@ -0,0 +1,55 @@
+//
+//          *                  *
+//             __                *
+//           ,db'    *     *
+//          ,d8/       *        *    *
+//          888
+//          `db\       *     *
+//            `o`_                    **
+//               *                 / )
+//             *    /\__/\ *       ( (  *
+//           ,-.,-.,)    (.,-.,-.,-.) ).,-.,-.
+//          | @|  ={      }= | @|  / / | @|o |
+//         _j__j__j_)     `-------/ /__j__j__j_
+//          ________(               /___________
+//          |  | @| \              || o|O | @|
+//          |o |  |,'\       ,   ,'"|  |  |  |  hjw
+//          vV\|/vV|`-'\  ,---\   | \Vv\hjwVv\//v
+//                     _) )    `. \ /
+//                    (__/       ) )
+//  _   _      _           _____                                            _
+// | \ | | ___| | _____   |  ___| __ __ _ _ __ ___   _____      _____  _ __| | __
+// |  \| |/ _ \ |/ / _ \  | |_ | '__/ _` | '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ /
+// | |\  |  __/   < (_) | |  _|| | | (_| | | | | | |  __/\ V  V / (_) | |  |   <
+// |_| \_|\___|_|\_\___/  |_|  |_|  \__,_|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\
+//
+//  IControllerContext.h
+//  Neko Framework
+//
+//  Copyright © 2018 Neko Vision. All rights reserved.
+//
+
+#pragma once
+
+#include "../../Engine/Network/Http/Request.h"
+#include "../../Engine/Network/Http/Response.h"
+
+#include "IController.h"
+
+namespace Neko
+{
+    namespace Mvc
+    {
+        /**
+         * Points to the function of a controller.
+         */
+        typedef TDelegate< void() > ControllerAction;
+        
+        struct IControllerContext
+        {
+            virtual IController* CreateController(Net::Http::Request& request, Net::Http::Response& response) = 0;
+            
+            virtual void InvokeAction(const char* name) = 0;
+        };
+    }
+}
